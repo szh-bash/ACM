0b31fe25def021652e68a78ba9e4ad5b9d1477a1
Immortals <342333349@qq.com>
2017-08-18 17:40:06 +0800

    ovo

--- BCPC2016/check.bat
+++
@@ -1,7 +0,0 @@
-:loop
-	data.exe
-	i2.exe
-	i3.exe
-	fc 1.out 1.std
-	if %errorlevel%==0 goto loop
-pause
--- BCPC2016/h_check .bat
+++
@@ -1,7 +0,0 @@
-:loop
-	data_h.exe
-	h.exe
-	h2.exe
-	fc h.out h.ans
-	if %errorlevel%==0 goto loop
-pause
