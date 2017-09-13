09300ec8a514d14b1f82e6c9d2ee3d3dc93563ae
Immortals <342333349@qq.com>
2017-09-13 23:49:50 +0800

    ovo

--- bzoj__Immortals__/权限题/check.bat
+++ bzoj__Immortals__/权限题/check.bat
@@ -1,7 +1,7 @@
 :loop
 	data.exe
-	p4017.exe
-	p4017_tangjz.exe
+	p1146.exe
+	p1146_整体二分.exe
 	fc 1.ans 1.out
 	if %errorlevel%==0 goto loop
 pause
--- bzoj__Immortals__/权限题/data.cpp
+++ bzoj__Immortals__/权限题/data.cpp
@@ -7,8 +7,22 @@ using namespace std;
 int main(){
 	srand(unsigned(time(NULL)));
 	freopen("1.in","w",stdout);
-	int n=10000;
-	cout<<n<<endl;
-	for (int i=1;i<=n;i++) printf("%d ",rand());cout<<endl;
+	int n=5, m=5;
+	cout<<n<<' '<<m<<endl;
+	for (int i=1;i<=n;i++) printf("%d ", rand());cout<<endl;
+	for (int i=2;i<=n;i++) printf("%d %d\n", i, rand()%(i-1)+1);
+	for (int i=1;i<=m;i++)
+		printf("%d %d %d\n", rand()%2, rand()%n+1, rand()%n+1);
 	return 0;
 }
+5 5
+1 2 3 4 5
+2 1
+3 1
+4 1
+5 3
+1 3 4
+0 1 1
+1 3 3
+0 4 4
+1 4 4
