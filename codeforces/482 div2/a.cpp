afcc4f9799e537b6ef5266648af6463268d79ab0
Immortals <342333349@qq.com>
2018-05-16 18:52:01 +0800

    ....

---
+++ codeforces/482 div2/a.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <algoriothm>
+using namespace std;
+int main(){
+	cin>>n;
+	n++;
+	n=sqrt(n);
+	return 0;
+}
