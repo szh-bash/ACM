a2e55f6f2f858659db3cf8b98581e8e1180e00ee
Immortal.S <342333349@qq.com>
2018-11-13 15:44:45 +0800

    Update ICPC2018/ACM-ICPC 2018 北京 onsite/icpcbj2018_j.cpp

--- ICPC2018/ACM-ICPC 2018 北京 onsite/icpcbj2018_j.cpp
+++ ICPC2018/ACM-ICPC 2018 北京 onsite/icpcbj2018_j.cpp
@@ -14,26 +14,18 @@ ull ts_a, ts_b;
 const ld pi=acos(-1);
 struct vec{
 	ull x, y;
-	double a;
+	ld a;
 }l[N], g[N];
 ll x[N], y[N];
 int n, cnt;
-ld get_sqrt(ull v){
-	ld le=0, ri=v, eps=1e-20;
-	while (ri-le>eps){
-		ld mid=(le+ri)/2;
-		if (mid*mid>v) ri=mid;else le=mid;
-	}
-	return le;
-}
-double get_angle(ull x, ull y){
-	double a=acos((double)x/get_sqrt(x*x+y*y));
+ld get_angle(ll x, ll y){
+/*	double a=acos(x/sqrt(x*x+y*y));
 	if (y<0) a=2*pi-a;
 	//cout<<x<<' '<<y<<' '<<a<<endl;
-	return a;
-/*	double t=atan2(y, x);
+	return a;*/
+	ld t=atan2(y, x);
 	if (t<0) t=2*pi+t;
-	return t;*/
+	return t;
 //	return atan2(y, x);
 }
 bool cmp(vec a, vec b){ return a.a<b.a;}
@@ -51,15 +43,9 @@ void calc_add(){
 	for (int i=1;i<cnt;i++){
 		while (mid<=cnt && l[mid].a-l[i].a<=pi) mid++;
 		ull tp=chaji_lg_b(i,i+1,mid-1)-chaji_lg_b(i,mid,cnt);
-		if (tp<0) tp=-tp;
-//		if (tp>=mo) tp-=mo;
-		tmp=(tmp+tp)%mo;
-//		if (tmp>=mo) tmp-=mo;
-//		cout<<(ll)l[i].x<<' '<<(ll)l[i].y<<' '<<l[i].a<<endl;
+		tmp=tmp+tp;
 	}
-//	cout<<(ll)tmp<<endl<<endl;
 	ts_a=(ts_a+tmp)%mo;
-//	if (ts_a>=mo) ts_a-=mo;
 	//cout<<(ll)tmp<<' '<<"lyk"<<endl;
 }
 void calc_sub(){
@@ -87,11 +73,9 @@ void calc_sub(){
 		ull tttt=0;
 		if (head<=mid && head<=tail) tttt=chaji_lg_b(i,head,min(mid-1,tail));
 		if (mid<=tail) tttt-=chaji_lg_b(i,mid,tail);
-		if (tttt<0) tttt=-tttt;
-//		if (tttt>=mo) tttt-=mo;
-		ts_b=(ts_b+tttt)%mo;
-//		if (ts_b>=mo) ts_b-=mo; 
+		ts_b=(ts_b+tttt);
 	}
+	ts_b%=mo;
 }
 ll power(ll x, int y){
 	ll c=1;
@@ -133,9 +117,9 @@ int main(){
 			calc_sub();
 		}
 		ts_a=ts_a*md%mo;
-//		cout<<(ll)ts_a<<' '<<(ll)ts_b<<endl;
 		ll ans=((ts_a-ts_b)%mo+mo)%mo;
 		cout<<ans<<endl;
+//		cout<<(ll)ts_a<<' '<<(ll)ts_b<<endl;
 	}
 	return 0;
 }
