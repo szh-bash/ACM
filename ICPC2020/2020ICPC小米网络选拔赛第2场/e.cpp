9bed9f96c4d13781f0b25963713ff640dd24115c
Immortal.S <342333349@qq.com>
2020-11-13 13:20:02 +0800

    upd

--- ICPC2020/2020ICPC小米网络选拔赛第2场/e.cpp
+++ ICPC2020/2020ICPC小米网络选拔赛第2场/e.cpp
@@ -32,8 +32,27 @@ ll readll(){
     while (num(ch)) p=p*10+ch-'0', ch=getchar();
     return p*q;
 }
+inline void ins(int x){
+    a[++cnt]=x;
+}
+void proceed(){
+    
+}
+void aswap(){
+}
+void solve(){
+    a[cnt=0]=-999999999;
+    for (int i=1;i<=n;i++) ins(x[i]=read()), ins(y[i]=read());
+    for (int i=1;i<=m;i++) ins(x[i+n]=read()), ins(y[i+n]=read());
+    sort(a+1,a+1+cnt);
+    for (int i=1;i<=cnt;i++)
+        if (a[i]!=a[i-1]) trans[mp[a[i]]=++ct]=a[i];
+    for (int i=1;i<=n+m;i++) fx[i]=mp[x[i]], fy[i]=mp[y[i]];
+    proceed();
+    aswap();
+    proceed();
+}
 int main(){
-	for (int _=read();_;_--){
-	}
+    for ((n=read()) && (m=read())) solve();
     return 0;
 }
